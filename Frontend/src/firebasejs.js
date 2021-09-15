b0540f34163ed8c3c8b4f9b12cc4b0ec61bc9695
sathiralamal <sdsat756@gmail.com>
2021-09-15 12:38:18 +0530

    update

--- Frontend/src/firebasejs.js
+++ Frontend/src/firebasejs.js
@@ -2,20 +2,18 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/database";
 import "firebase/storage";
-import "firebase/analytics";
 import firebase from "firebase/app";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeUwj2JMq2m_cn-u3jjFKEl-Yo3mh2Plo",
-  authDomain: "ticketing-seystem.firebaseapp.com",
-  databaseURL: "https://ticketing-seystem-default-rtdb.firebaseio.com",
-  projectId: "ticketing-seystem",
-  storageBucket: "ticketing-seystem.appspot.com",
-  messagingSenderId: "813235961661",
-  appId: "1:813235961661:web:3354c906bd54c1b0dc0f54",
-  measurementId: "G-BR5FGV9TMV"
-
+    apiKey: "AIzaSyAjS-NRIKCIYOh5zLQg4mE3dLoWFSzBXcM",
+    authDomain: "ticketsystem-e895b.firebaseapp.com",
+    databaseURL: "https://ticketsystem-e895b.firebaseio.com",
+    projectId: "ticketsystem-e895b",
+    storageBucket: "ticketsystem-e895b.appspot.com",
+    messagingSenderId: "195590015564",
+    appId: "1:195590015564:web:aaaca6fba7d06801e642e9",
+    measurementId: process.env.REACT_APP_MeasurementId
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -23,7 +21,6 @@ const auth =firebase.auth();
 const firestore=firebase.firestore();
 const database=firebase.database();
 const storage = firebase.storage();
-const analytics = firebase.analytics();
+
 
 export {auth,firebase,firestore,database,storage};
-
