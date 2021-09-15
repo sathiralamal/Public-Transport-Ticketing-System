387310bb6deeeb98e6043fe17a72095aff6be0c8
sathiralamal <sdsat756@gmail.com>
2021-09-15 12:27:23 +0530

    add db link

--- Frontend/src/firebasejs.js
+++ Frontend/src/firebasejs.js
@@ -7,13 +7,14 @@ import firebase from "firebase/app";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDeUwj2JMq2m_cn-u3jjFKEl-Yo3mh2Plo",
-    authDomain: "ticketing-seystem.firebaseapp.com",
-    projectId: "ticketing-seystem",
-    storageBucket: "ticketing-seystem.appspot.com",
-    messagingSenderId: "813235961661",
-    appId: "1:813235961661:web:3354c906bd54c1b0dc0f54",
-    measurementId: process.env.REACT_APP_MeasurementId
+  apiKey: "AIzaSyDeUwj2JMq2m_cn-u3jjFKEl-Yo3mh2Plo",
+  authDomain: "ticketing-seystem.firebaseapp.com",
+  databaseURL: "https://ticketing-seystem-default-rtdb.firebaseio.com",
+  projectId: "ticketing-seystem",
+  storageBucket: "ticketing-seystem.appspot.com",
+  messagingSenderId: "813235961661",
+  appId: "1:813235961661:web:3354c906bd54c1b0dc0f54",
+  measurementId: "G-BR5FGV9TMV"
 
 };
 
